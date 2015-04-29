cb8b571702382331215e5e0f7f4ebf68cd247d90
oslet <oslet@oslet.net>
2015-04-29 21:16:53 +0800

    fs error pipe unpip

---
+++ 6.44.js
@@ -0,0 +1,4 @@
+var fs=require('fs');
+var file=fs.createReadStream('./message.txt');
+var out=fs.createWriteStream('./anotherMessage.txt');
+file.pipe(out);
---
+++ 6.46.js
@@ -0,0 +1,8 @@
+var fs=require('fs');
+var file=fs.createReadStream('./crash.mp3');
+var out=fs.createWriteStream('./anotherCrash.mp3');
+file.pipe(out,{end:false});
+setTimeout(function(){
+  file.unpipe(out);
+  out.end();
+},10);
