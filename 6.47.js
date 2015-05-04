0bfe7904eb1913e37cf8f96ac532f19c2e8cb726
oslet <oslet@oslet.net>
2015-05-04 21:50:12 +0800

    path useful

---
+++ 6.47.js
@@ -0,0 +1,8 @@
+var fs=require('fs');
+var path=require('path');
+var myPath=path.normalize('.//a//b//d//..//c/e//..//');
+console.log(myPath);
+var file=fs.createReadStream(myPath+'message.txt');
+file.on('data',function(data){
+  console.log(data.toString());
+});
