c154b79cb46e8b3f4b46c7c6aef921e2d9675b2b
oslet <oslet@oslet.net>
2015-05-10 10:10:50 +0800

    socket some usage

---
+++ 7.10.js
@@ -0,0 +1,11 @@
+var net=require('net');
+var file=require('fs').createWriteStream('./message.txt');
+var server=net.createServer();
+server.on('connection',function(socket){
+  socket.pause();
+  setTimeout(function(){
+    socket.resume();
+    socket.pipe(file);
+  },30000);
+});
+server.listen(8431,'localhost');
---
+++ 7.11.js
@@ -0,0 +1,12 @@
+var net=require('net');
+var file=require('fs').createWriteStream('./message.txt');
+var server=net.createServer();
+server.on('connection',function(socket){
+  socket.setTimeout(10*1000);
+  socket.pause();
+  socket.on('timeout',function(){
+    socket.resume();
+    socket.pipe(file);
+  });
+});
+server.listen(8431,'localhost');
