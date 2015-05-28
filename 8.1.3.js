75a875db2b7f712bb2667289253a1249b040e418
oslet <oslet@oslet.net>
2015-05-28 22:15:08 +0800

    url parse etc

--- 8.1.3.js
+++ 8.1.3.js
@@ -66,3 +66,57 @@ url.parse(urlStr,[parseQueryString])
   pathname: '/users/user.php',
   path: '/users/user.php?username=lulingniu&age=40&sex=male',
   href: 'http://user:pass@host.com:8080/users/user.php?username=lulingniu&age=40&sex=male#name1' }
+
+url.format(urlObj)
+
+  >obj=url.parse('http://user:pass@host.com:8080/users/user.php?userName=lulingniu&age=40&sex=male#name1')
+{ protocol: 'http:',
+  slashes: true,
+  auth: 'user:pass',
+  host: 'host.com:8080',
+  port: '8080',
+  hostname: 'host.com',
+  hash: '#name1',
+  search: '?userName=lulingniu&age=40&sex=male',
+  query: 'userName=lulingniu&age=40&sex=male',
+  pathname: '/users/user.php',
+  path: '/users/user.php?userName=lulingniu&age=40&sex=male',
+  href: 'http://user:pass@host.com:8080/users/user.php?userName=lulingniu&age=40&sex=male#name1' }
+
+  >url.format(obj)
+'http://user:pass@host.com:8080/users/user.php?userName=lulingniu&age=40&sex=male#name1'
+
+url.resolve(from,to)
+
+  >url.resolve('http://www.example.com/a/b/c','/one')
+'http://www.example.com/one'
+
+  >url.resolve('http://www.example.com/a/b/c','http://www.google.com/')
+'http://www.google.com/'
+
+  > url.resolve('http://www.example.com','one')
+'http://www.example.com/one'
+
+  > url.resolve('http://www.example.com/','one')
+'http://www.example.com/one'
+
+  > url.resolve('http://www.example.com','./one')
+'http://www.example.com/one'
+
+  > url.resolve('http://www.example.com/a/b/c','one')
+'http://www.example.com/a/b/one'
+
+  > url.resolve('http://www.example.com/a/b/c/','one')
+'http://www.example.com/a/b/c/one'
+
+  > url.resolve('http://www.example.com/a/b/c','./one')
+'http://www.example.com/a/b/one'
+
+  > url.resolve('http://www.example.com/a/b/c/','./one')
+'http://www.example.com/a/b/c/one'
+
+  > url.resolve('http://www.example.com/a/b/c','../one')
+'http://www.example.com/a/one'
+
+  > url.resolve('http://www.example.com/a/b/c/','../one')
+'http://www.example.com/a/b/one'
