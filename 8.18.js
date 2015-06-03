24c608c1819792341fdb1cecd1159b709188dde7
oslet <oslet@oslet.net>
2015-06-03 22:16:45 +0800

    http response close

---
+++ 8.18.js
@@ -0,0 +1,12 @@
+var http=require('http');
+var server=http.createServer(function(req,res){
+  if(req.url!=="/favicon.ico"){
+    res.on('close',function(){console.log('连接中断.');});
+    setTimeout(function(){
+      res.setHeader('Content-Type','text/html');
+      res.write('<html><head><meta charset="utf-8"/></head>');
+      res.write("你好");
+      res.end();
+    },5000);
+  }
+}).listen(1337,"localhost");
