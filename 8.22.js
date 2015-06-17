4ed133ce23def7abbc682d2a479569aaf24daa43
oslet <oslet@oslet.net>
2015-06-17 21:19:22 +0800

    http req proxy

--- 8.22.js
+++ 8.22.js
@@ -3,7 +3,13 @@ var server=http.createServer(function(req,res){
   if(req.url!=="/favicon.ico"){
     req.on('data',function(data){
       console.log('服务器端接收到的数据: '+data);
-      res.end();
+      res.write('确认数据: '+data);
+      //      req.on('end',function(){res.end();})
+      req.on('end',function(){
+        res.addTrailers({'Content-MD5':'7895bf4b8828b55ceaf47747b4bca667'})
+        res.end();
+      })
+//      res.end();
     });
   }
 }).listen(1337,"localhost");
--- 8.23.js
+++ 8.23.js
@@ -5,6 +5,13 @@ var options={
   path: '/',
   method: 'POST'
 };
-var req=http.request(options);
+var req=http.request(options,function(res){
+  res.on('data',function(chunk){
+    console.log('客户端接收到数据:'+chunk);
+  });
+  res.on('end',function(){
+    console.log('Trailer头信息:%j',res.trailers);
+  });
+  });
 req.write('你好');
 req.end('再见');
